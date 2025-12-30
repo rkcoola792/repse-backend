c264439fdcf668a06aad01b004a1efd488522da8
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-12-30 13:03:03 +0530

    completed payment gateaway

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,7 +3,7 @@ const paymentRouter = express.Router();
 const razorpayInstance = require("../utils/razorpay");
 const userAuth = require("../middlewares/auth");
 const Order = require("../models/payment");
-
+const validateWebhookSignature = require("razorpay/dist/utils/razorpay-utils");
 paymentRouter.post("/create-order", userAuth, async (req, res) => {
   try {
     const { amount, currency, receipt, notes } = req.body;
@@ -33,12 +33,38 @@ paymentRouter.post("/create-order", userAuth, async (req, res) => {
     });
 
     const savedOrder = await newOrder.save();
-    res
-      .status(200)
-      .json({
-        ...savedOrder.toJSON(),
-        keyId: process.env.RAZORPAY_TEST_KEY_ID,
-      });
+    res.status(200).json({
+      ...savedOrder.toJSON(),
+      keyId: process.env.RAZORPAY_TEST_KEY_ID,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error: " + error.message });
+  }
+});
+paymentRouter.post("/payment/webhook", async (req, res) => {
+  try {
+    const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
+    const signature = req.headers["X-Razorpay-Signature"];
+    const isWebhookValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      signature,
+      process.env.RAZORPAY_WEBHOOK_SECRET
+    );
+    if (!isWebhookValid) {
+      return res.status(400).json({ error: "Invalid webhook signature" });
+    }
+
+    //update my payment details in database
+    const paymentDetails = req.body.payload.payment.entity;
+    const paymentOrder = await Order.findOne({
+      orderId: paymentDetails.order_id,
+    });
+
+    paymentOrder.status = paymentDetails.status;
+    await paymentOrder.save();
+
+    //return scuccess response to razorpay
+    res.status(200).json({ status: "ok" });
   } catch (error) {
     res.status(500).json({ error: "Server error: " + error.message });
   }
